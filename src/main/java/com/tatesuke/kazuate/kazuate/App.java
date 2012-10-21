b1249a76e58e26ed1638598239f3c21cd51a42ba
tatesuke <acousticdays@gmail.com>
2012-10-21 22:23:43 +0900

    first commit

---
+++ src/main/java/com/tatesuke/kazuate/kazuate/App.java
@@ -0,0 +1,13 @@
+package com.tatesuke.kazuate.kazuate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
